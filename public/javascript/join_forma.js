76326f87752630f0aca1754feea0a5d491367e01
Nasko9 <53650219+Nasko9@users.noreply.github.com>
2021-01-15 18:49:25 +0100

    commit
    Dodato je sortiranje (pagitation) na jednoj strani se sortira po 5 najnovijih akcija i dogadjaja i tako u beskonacnost

---
+++ public/javascript/join_forma.js
@@ -0,0 +1,56 @@
+//Provera forme
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+//KONTAKT SETUP
+const kontaktForma = document.querySelector('#join');
+let ime = document.getElementById('ime')
+let prezime = document.getElementById('prezime');
+let email = document.getElementById('email');
+let motivacija = document.getElementById('motivacija');
+let stanje = document.getElementById('stanje');
+
+kontaktForma.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let formData = {
+        ime: ime.value,
+        prezime: prezime.value,
+        email: email.value,
+        motivacija: motivacija.value,
+        stanje: stanje.value
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/pridruzi_se');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        if(xhr.responseText == 'success'){
+            alert('Email jeuspesno poslat');
+            ime.value = '';
+            prezime.value = '';
+            email.value = '';
+            motivacija.value = '';
+            stanje.value = '';
+        } else{
+            alert('Nesto nije uredu');
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+});
